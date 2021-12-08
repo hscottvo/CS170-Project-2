8a0bb847f2fd641ebbfd7c0cab53624723be6c9c
Scott <hscottvo@gmail.com>
2021-12-07 16:10:11 -0800

    Output logs of the order in which features are added

--- main.cpp
+++ main.cpp
@@ -1,87 +1,21 @@
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <sstream>
+
 #include <vector>
 #include <algorithm>
 #include <limits>
 #include <unordered_set>
 #include <math.h>
 
+#include "util.h"
+
 using namespace std;
 
 // template <typename T>
 
-// https://thispointer.com/c-test-check-if-a-value-exist-in-vector/
-bool contains(unordered_set<unsigned int>& s, const unsigned int & elem)
-{
-    bool result = false;
-    // if( find(vec.begin(), vec.end(), elem) != vec.end() )
-    // {
-    //     result = true;
-    // }
-    if (s.find(elem) != s.end()) {
-        result = true;
-    }
-    return result;
-}
-
-template <typename T>
-void print(unordered_set<T>& s, bool endline){
-    // for(unsigned int i = 0; i < vec.size(); ++i) {
-    //     cout << vec[i] << ' ';
-    // }
-    for (auto const &i: s) {
-        std::cout << i << " ";
-    }
-    if (endline) cout << endl;
-}
-
-template <typename T>
-void print(vector<T>& vec, bool endline) {
-    for(unsigned int i = 0; i < vec.size(); ++i) {
-        cout << vec[i] << ' ';
-    }
-    cout << endl;
-}
-
-template <typename T>
-void print(vector<T>& vec, bool endline, int index){
-    for(unsigned int i = index; i < vec.size(); ++i) {
-        cout << vec[i] << ' ';
-    }
-    if (endline) cout << endl;
-}
 
 
-vector<double> get_data(vector<double>& row, unordered_set<unsigned int>& features) {
-    vector<double> ret;
-    // cout << "row: ";print(row, true);
-    for(unsigned int i = 1; i < row.size(); ++i) {
-        if (contains(features, i)) {
-            ret.push_back(row[i]);
-        } else {
-            ret.push_back(0);
-        }
-    }
-    return ret;
-}
 
-vector<double> parse_line(string& in) {
-    vector<double> out;
-    stringstream ss(in);
-    string curr;
-    while(true) {
-        if (ss >> curr){
-            out.push_back(stod(curr));
-
-        } else {
-            break;
-        }
-    }
-
-    return out;    
-}
 
 // double euclidean_distance(vector<double>& train, vector<double>& test){
 //     double sum = 0;
@@ -93,36 +27,7 @@ vector<double> parse_line(string& in) {
 //     return sqrt(sum);
 // }
 
-double euclidean_distance(vector<vector<double> >& data, unsigned int i, unsigned int k, unordered_set<unsigned int> feature_set, unsigned int new_feature){
-    
-    double sum = 0;
-    double a = 0;
-    double b = 0;
-    for(size_t j = 1; j < data[0].size(); ++j) {
-        // cout << "train[" << i << "]: " << train[i] << "\ttest[" << i << "]: " << test[i] << endl;
-        // cout << "Feature set: "; print(feature_set, true);
-        // cout << "features: ";
-        if (contains(feature_set, j)){
-            // cout << "data[i][j]: " << data[i][j] << endl;
 
-            // cout << j << " ";
-            sum += (data[i][j] - data[k][j]) * (data[i][j] - data[k][j]);
-        }
-        // cout << endl;
-    }
-    sum += (data[i][new_feature] - data[k][new_feature]) * (data[i][new_feature] - data[k][new_feature]);
-    // vector<double> test;
-    // for(size_t j = 1; j < data[i].size(); ++j) {
-    //     if(contains(feature_set, j)){
-    //         test.push_back(data[i][j]);
-    //     } else {
-    //         test.push_back(-1);
-    //     }
-    // }
-    // print(test, true);
-    // cout << sqrt(sum) << endl;
-    return sqrt(sum);
-}
 
 double cross_val(vector<vector<double> >& in, unordered_set<unsigned int>& feature_set, unsigned int feature_to_add, const int rows, const int cols){
     int correct_count = 0;
@@ -172,7 +77,7 @@ double cross_val(vector<vector<double> >& in, unordered_set<unsigned int>& featu
 }
 
 
-void feature_search(vector<vector<double> >& in, const int rows, const int cols){
+void feature_search_forward(vector<vector<double> >& in, const int rows, const int cols, vector<unordered_set<unsigned int> >& feature_list, vector<double>& feature_accs){
     unordered_set<unsigned int> feature_set;
     double main_best_acc = 0;
     unordered_set<unsigned int> final_set;
@@ -200,6 +105,51 @@ void feature_search(vector<vector<double> >& in, const int rows, const int cols)
         }
 
         feature_set.insert(feature_to_add);
+        feature_list.push_back(feature_set);
+        feature_accs.push_back(best_so_far_acc);
+
+        cout << "On level " << i << ", added feature " << feature_to_add << " to current set" << endl;
+        // print(feature_set, true);
+        if (best_so_far_acc > main_best_acc) {
+            main_best_acc = best_so_far_acc;
+            final_set = feature_set;
+        }
+        cout << "So-far best feature set with accuracy " << main_best_acc << ": "; print(final_set, true);
+    }
+    cout << "Overall best feature set: "; print(final_set, true);
+}
+
+void feature_search_backward(vector<vector<double> >& in, const int rows, const int cols, vector<unordered_set<unsigned int> >& feature_list, vector<double>& feature_accs){
+    unordered_set<unsigned int> feature_set;
+    double main_best_acc = 0;
+    unordered_set<unsigned int> final_set;
+
+    for(unsigned int i = 1; i < cols; i++){
+        // cout << "On the " << i << "th level of the search tree" << endl;
+        int feature_to_add = -1;
+        double best_so_far_acc = 0;
+        double acc = 0;
+        for (unsigned int k = 1; k < cols; k++) {
+            if (contains(feature_set, k)) {
+                continue;
+                // cout << "this should not print" << endl;
+            }
+            cout << "\tConsidering adding the " << k << "th feature, seeing if ";
+            print(feature_set, false);
+            cout << k << " works" << endl;
+            acc = cross_val(in, feature_set, k, rows, cols);
+            cout << "\t\tThis-set Accuracy: " << acc << endl << endl;
+
+            if (acc > best_so_far_acc) {
+                best_so_far_acc = acc;
+                feature_to_add = k;
+            }
+        }
+
+        feature_set.insert(feature_to_add);
+        feature_list.push_back(feature_set);
+        feature_accs.push_back(best_so_far_acc);
+
         cout << "On level " << i << ", added feature " << feature_to_add << " to current set" << endl;
         // print(feature_set, true);
         if (best_so_far_acc > main_best_acc) {
@@ -218,11 +168,16 @@ int main(int argc, char** argv)
         cout << "Too many arguments. Exiting..." << endl;
         exit(1);
     }
+    string filename = argv[1];
+    filename = filename.substr(0, filename.find('.'));
     ifstream fs(argv[1]);
     if(!fs.is_open()){
         cout << "Cannot read file. Exiting..." << endl;
         exit(1);
     }
+
+    vector<unordered_set<unsigned int> > feature_list;
+    vector<double> feature_accs;
     string in;
     int label_1_count = 0;
     vector<vector<double> > table;
@@ -233,15 +188,37 @@ int main(int argc, char** argv)
     }
     int rows = table.size();
     int cols = table[0].size();
-    cout << "Default rate: ";
+    double default_rate;
     if(label_1_count * 2 > table.size()){
-        cout << label_1_count/float(table.size());
+        default_rate = label_1_count/float(table.size());
     } else {
-        cout << (table.size()-label_1_count)/float(table.size());
+        default_rate =  (table.size()-label_1_count)/float(table.size());
     }
-    cout << endl;
-    feature_search(table, rows, cols);
+    cout << "Default rate: " << default_rate << endl;
+    unordered_set<unsigned int> empty_set;
+    feature_list.push_back(empty_set);
+    feature_accs.push_back(default_rate);
+    feature_search_forward(table, rows, cols, feature_list, feature_accs);
 
+
+    ofstream outfile(filename + "_logs.csv");
+    // cout << "Filename: " << filename << endl;
+    cout << "Final accuracies: " << endl;
+    for(unsigned int i = 0; i < feature_list.size(); ++i) { 
+        cout << "\tAccuracy: " << feature_accs[i] << " with features ";
+        outfile << to_string(feature_accs[i]) << ", ";
+        if (feature_list[i].size()) {
+            print(feature_list[i], true);
+            outfile << tostring(feature_list[i]) << ", " << endl;
+        } 
+        else {
+            cout << "___" << endl;
+            outfile << "empty set" << ", " << endl;
+        }
+
+    
+    }
+    outfile.close();
     fs.close();
 
 	return 0;
